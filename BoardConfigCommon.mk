8dd28e9cd7caa4144f050dd27025c08be4ea4bd7
Aayush Gupta <aayushgupta219@gmail.com>
2021-10-31 04:31:32 +0000

    DNM: sdm660-common: Comment out qcom sepolicy
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -140,7 +140,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
