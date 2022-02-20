960ea09009bb5ae40b2f774b69d7e9b64827c2e4
Apavayan Sinha <info@apavayan.com>
2022-02-20 20:12:17 +0530

    sdm660-common: Add CTL to guards
    Change-Id: I7e6f84df32f8e9eb4ef20ca0149abc81053b3d96

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter DDV DRG PL2,$(TARGET_DEVICE)),)
+ifneq ($(filter CTL DDV DRG PL2,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
